18f2d3ae24336db23e7e4e0a5c395f6cb7c6b516
Hedrick Matamorosa <hedrickalbertmatamorosa@gmail.com>
2026-09-18 09:20:47 +0000

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/companionspotify/backend/CompanionSpotifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.companionspotify.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionSpotifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionSpotifyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/companionspotify/backend/CompanionSpotifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companionspotify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionSpotifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
